228fb6a91699d6b6e547bcd455bbd129b148a021
ibkkkkk <yuikuro827@gmail.com>
2023-01-22 14:55:37 +0000

    improve SP and spec

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
     namespace :staff, path: config[:staff][:path] do
       root "top#index"
       get "login" => "sessions#new", as: :login
-      resources :session, only: [:create, :destroy]
-      resource :account, except: [:new, :create, :destroy]
+      resource :session, only: [:create, :destroy]
+      resource :account, except: [ :new, :create, :destroy]
     end
   end
 
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
     namespace :admin, path: config[:admin][:path] do
       root "top#index"
       get "login" => "sessions#new", as: :login
-      resource :session, only:[:create, :destroy]
+      resource :session, only: [:create, :destroy]
       resources :staff_members
     end
   end
